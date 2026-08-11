b1e00c1652aa50a084c5e86c99add63cb48f0cc2
Filipe Marchesini <filipe.marchesini@gmail.com>
2026-08-11 17:33:13 -0300

    feat: expose the Baby Lights package entry point

--- baby_lights/__init__.py
+++ baby_lights/__init__.py
@@ -2,6 +2,15 @@ from baby_lights.app import BabyLightsApp
 from baby_lights.logger import logger
 
 __all__ = ['BabyLightsApp']
-__version__ = '0.1.0'
+__version__ = '0.1.1'
+
+
+def main():
+    """Run Baby Lights through the same entry point on desktop and Android."""
+    import trio
+
+    app = BabyLightsApp()
+    trio.run(app.async_run, 'trio')
+
 
 logger.info(f'Baby Lights App version {__version__} initialized.')
