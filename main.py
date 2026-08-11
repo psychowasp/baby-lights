a22056244873c4febd5bfe32f2da7f1ebf4347db
Filipe Marchesini <filipe.marchesini@gmail.com>
2026-08-11 17:33:13 -0300

    dev: select the hot-reload app launcher

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import os
 import trio
+
+# The package/Android entry point does not set this variable and therefore
+# uses plain Kivy. This launcher is intentionally the development entry point.
+os.environ.setdefault('BABY_LIGHTS_MODE', 'development')
 
 from baby_lights import BabyLightsApp
 
